4aa224be1495b1f214b70e14a9443f32fc37a7d0
@jamgorilla <jamesmurphy507@hotmail.com>
2018-08-15 18:46:45 +0100

    First commit of always latest

---
+++ public/map.js
@@ -0,0 +1,63 @@
+var map;
+var currentCenter;
+
+var lastPosition = {lat: 0,lng: 0};
+
+
+function initCurrentMap(geoLat, geoLon){
+  
+        geoLat = geoLat || 51.5255000;
+        geoLon = geoLon || -0.0795000;
+
+      currentCenter = {lat: geoLat,lng: geoLon};
+ 
+      //map options
+      var options = {
+        zoom: 11,
+        center: {lat: geoLat,lng: geoLon}
+      }
+
+      //new map
+      map = new google.maps.Map(document.getElementById('map'), options);
+
+      //callback to initiate markers
+      map_panels_populate(function(loc_array){
+
+        for (var p = 0;p < loc_array.length;p++) {
+
+          var temp_latlng = loc_array[p].split(":");
+          var latitude = Number(temp_latlng[0]);
+          var longitude = Number(temp_latlng[1]); 
+
+          // Add Markers
+          let lastKnownLocation = new google.maps.Marker({
+            position: {lat: latitude,lng: longitude},
+            map: map,
+            animation: google.maps.Animation.DROP
+            // icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+          })
+        }
+      })
+    
+    }
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+//These two functions perform a recet on the maps in the tabs so that they both appear
+function ResetMap1() {
+     //setTimeout(function(){
+    //google.maps.event.trigger(map, 'resize');
+    map.setCenter(currentCenter);
+    map.setZoom(11);
+    // window.location.hash = '#currentLoc';
+    // }, 500);
+}
+
+// function ResetMap2() {
+//    setTimeout(function(){
+//       google.maps.event.trigger(map2, 'resize');
+//  map2.setCenter(currentCenter);
+//  map.setZoom(11);
+//    window.location.hash = '#prevLoc';
+//   }, 500);
+// }
---
+++ public/search_bar.js
@@ -0,0 +1,38 @@
+function searchFunction(event) {
+
+  var input = document.getElementById('myinput');
+
+  //send to google maps api for geolocation lat lng
+
+  console.log(input.value, locations_array)
+
+  //console.log(event)
+  
+
+  var filter = document.getElementById('class');
+
+  var title = document.getElementById('store_box_titles');
+
+}
+
+
+
+
+  var rad = function(x) {
+    return x * Math.PI / 180;
+  };
+
+  var getDistance = function(p1, p2) {
+    var R = 6378137; // Earth’s mean radius in meter
+    var dLat = rad(p2.lat() - p1.lat());
+    var dLong = rad(p2.lng() - p1.lng());
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(rad(p1.lat())) * Math.cos(rad(p2.lat())) *
+      Math.sin(dLong / 2) * Math.sin(dLong / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+    return d; // returns the distance in meter
+  };
+
+
+  console.log()
